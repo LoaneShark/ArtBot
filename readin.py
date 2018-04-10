72d5f9e2c90013a31b3dc2a232692d12e60e4d47
Santiago Loane <santiago.loane@rochester.edu>
2018-04-09 20:16:21 -0400

    Kaggle Painter by Numbers
    Dataset is too large to include in git. It is currently assumed to be in
    the same folder as the ArtBot directory.

---
+++ readin.py
@@ -0,0 +1,26 @@
+####################################
+# Santiago Loane & Brad Beyers
+# CSC 249
+# 
+# v0.0		4/4/18
+####################################
+
+# Reads in the paintings
+
+# TODO: - everything
+
+import numpy as np 
+import scipy
+import gzip
+
+
+# establish path to wherever the kaggle database is
+# stored locally (uncompressed)
+kagglepath = "../kaggle/"
+
+def main():
+	x = 5
+
+
+if __name__ == "__main__":
+	main()
